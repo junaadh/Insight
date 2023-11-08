58350ac6e47c93202d2abd7c86eed062b159a03a
Moosa Junaadh <junaadh.02@gmail.com>
2023-11-08 23:39:14 +0800

    filepicker for db working

--- src/main/java/aps/App.java
+++ src/main/java/aps/App.java
@@ -9,35 +9,41 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+import db.FilePicker;
+import db.FilePicker.dbFiles;
+import java.io.File;
+
 /**
  * JavaFX App
  */
 public class App extends Application {
 
-    private static Scene scene;
+  private static Scene scene;
 
-    @Override
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("landing"), 1220, 720);
-        stage.setTitle("Insight");
-        Image icon = new Image(getClass().getResource("pics/icampus_icon-removebg-preview-2.png").toExternalForm());
-        stage.getIcons().add(icon);
-        stage.setScene(scene);
-        stage.setResizable(false);
-        stage.show();
-    }
+  @Override
+  public void start(Stage stage) throws IOException {
+    scene = new Scene(loadFXML("landing"), 1220, 720);
+    stage.setTitle("Insight");
+    Image icon = new Image(getClass().getResource("pics/icampus_icon-removebg-preview-2.png").toExternalForm());
+    stage.getIcons().add(icon);
+    stage.setScene(scene);
+    stage.setResizable(false);
+    stage.show();
+  }
 
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
+  static void setRoot(String fxml) throws IOException {
+    scene.setRoot(loadFXML(fxml));
+  }
 
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
-    }
+  private static Parent loadFXML(String fxml) throws IOException {
+    FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+    return fxmlLoader.load();
+  }
 
-    public static void main(String[] args) {
-        launch();
-    }
+  public static void main(String[] args) {
+    launch();
+    // File testing = FilePicker.getdbFile(dbFiles.USERS);
+    // System.out.println(testing);
+  }
 
 }
--- src/main/java/db/FilePicker.java
+++ src/main/java/db/FilePicker.java
@@ -1,33 +1,60 @@
 package db;
 
 import java.io.File;
+import java.io.IOException;
 
 public class FilePicker {
 
-  // private String filePath;
-
-  public FilePicker() {
-  }
-
   public enum dbFiles {
+    USERS("users.bin"),
+    ADMINS("admins.bin"),
+    SURVEY_CREATORS("admins.bin"),
+    SURVEYS("surveys.bin"),
+    REVIEWS("reviews.bin");
 
+    private final String fileName;
+
+    dbFiles(String filename) {
+      this.fileName = filename;
+    }
+
+    public String getFilename() {
+      return fileName;
+    }
   }
 
-  private File getDbDir() {
-    String dbDir = "/database";
+  private static File getDbDir() {
+    String root = System.clearProperty("user.dir");
+    String dbDir = root + "/database";
 
     File dir = new File(dbDir);
 
     if (!dir.exists()) {
       try {
-        new File(dbDir).mkdir();
+        dir.mkdir();
       } catch (SecurityException e) {
-        // TODO 
-        System.out.println("Hi tis a checkpoint");
+        // TODO
+        System.out.println("Error creating directory: " + e.getMessage());
       }
     }
 
     return dir;
+  }
+
+  public static File getdbFile(dbFiles type) {
+    File dbDir = getDbDir();
+    String filename = type.getFilename();
+    File db = new File(dbDir, filename);
+
+    if (!db.exists()) {
+      try {
+        db.createNewFile();
+      } catch (IOException e) {
+        System.out.println("Error creating db: " + e.getMessage());
+      }
+    }
+
+    return db;
   }
 
 }
